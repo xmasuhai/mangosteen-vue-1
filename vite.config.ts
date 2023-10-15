0f94453bc13b7e7e46d596f6ecade2cee6afcdb0
xmasuhai <xmasuhai@163.com>
2023-10-15 17:07:40 +0800

    改回 默认base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mangosteen-vue-1/',
+  // base: '/mangosteen-vue-1/',
   plugins: [vue()],
 })
